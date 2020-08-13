c5c6328d6ed5bce37dd8764011f4e1e4bb7b5d0e
Oleksandr Dykyi <o.dykyi@netz98.de>
2020-08-13 17:33:43 +0200

    Updated SQL dump

--- mysql-dump/create.sql
+++ mysql-dump/create.sql
@@ -30,21 +30,31 @@ CREATE TABLE messages
 (
     message_id    INT PRIMARY KEY AUTO_INCREMENT,
     message_text  TEXT,
-    for_all       CHAR(1) DEFAULT '1',
+    for_all       CHAR(1)  DEFAULT '1',
     time_creation DATETIME DEFAULT current_timestamp(),
-    is_new4admin  CHAR(1) DEFAULT '0',
+    is_new4admin  CHAR(1)  DEFAULT '0',
     author_name   VARCHAR(80),
     author_ip     VARCHAR(30),
     phone         VARCHAR(80),
     email         VARCHAR(80),
-    icq           VARCHAR(40),
     answer_text   TEXT,
     moderator_id  INT SIGNED NOT NULL,
     time_answer   DATETIME,
     guestbook_id  INT SIGNED NOT NULL,
 
-    CONSTRAINT fk_messages_moderator_id_moderators_moderator_id FOREIGN KEY (moderator_id) REFERENCES moderators (moderator_id) ON DELETE CASCADE,
     CONSTRAINT fk_messages_guestbook_id_guestbooks_guestbook_id FOREIGN KEY (guestbook_id) REFERENCES guestbooks (guestbook_id) ON DELETE CASCADE
+);
+
+CREATE TABLE answers
+(
+    answer_id    INT PRIMARY KEY AUTO_INCREMENT,
+    answer_text  TEXT,
+    moderator_id INT SIGNED NOT NULL,
+    time_answer  DATETIME,
+    message_id   INT SIGNED NOT NULL,
+
+    CONSTRAINT fk_messages_moderator_id_moderators_moderator_id FOREIGN KEY (moderator_id) REFERENCES moderators (moderator_id) ON DELETE CASCADE,
+    CONSTRAINT fk_messages_message_id_messages_message_id FOREIGN KEY (message_id) REFERENCES messages (message_id) ON DELETE CASCADE
 );
 
 INSERT INTO users (user_id, username, pwd_digest, is_admin) values (1, 'root', 'yrcdZab1wbOKbOgV5TYcpU4/qYwjryzJJKMW00E0jYE=', '1');
